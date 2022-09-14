fa0da937824cd854ca0b220f206217020efde6b9
Michael Klatt <mdklatt@alumni.ou.edu>
2022-09-14 11:59:52 -0500

    Moved project settings to gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-group = "dev.mdklatt"
-version = "0.1.0.dev0"
-
 plugins {
     id("org.jetbrains.kotlin.jvm") version("1.7.10")
     id("org.jetbrains.intellij") version("1.8.0")
@@ -14,23 +11,27 @@ repositories {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     testImplementation(kotlin("test"))
+
+    // JUnit3 (junit-vintage) is required for running IDEA platform tests.
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
 
-intellij {
-    type.set("IC")
-    version.set("2022.1")
-}
-
 tasks {
     wrapper {
         gradleVersion = "7.5.1"
     }
-
     test {
         useJUnitPlatform()
     }
+}
+
+intellij {
+    // Even though this is not an IDEA plugin project, this is required to
+    // resolve the platform APIs used by this library.
+    val platformVersion: String by project  // see gradle.properties
+    version.set(platformVersion)
+    type.set("IC")  // IntelliJ Community
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
+// This is also used as the Maven artifact ID, and changing it will break
+// downstream projects.
+// <https://maven.apache.org/pom.html#maven-coordinates>
 rootProject.name = "idea-common"

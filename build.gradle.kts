323a1c113772867d77370e661dcc3c43c94b2e2d
Michael Klatt <mdklatt@alumni.ou.edu>
2022-09-18 23:38:54 -0500

    Tweaked build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version("1.7.10")
+    kotlin("jvm") version("1.7.10")
     id("org.jetbrains.intellij") version("1.9.0")
     id("java-library")
     id("maven-publish")
 }
 
+
 repositories {
     mavenCentral()
 }
+
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -18,19 +20,21 @@ dependencies {
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
 
+
 tasks {
     wrapper {
         gradleVersion = "7.5.1"
     }
 }
 
+
 intellij {
     // Even though this is not an IDEA plugin project, this is required to
     // resolve the platform dependencies used by this library.
     val platformVersion: String by project  // see gradle.properties
     version.set(platformVersion)
-    type.set("IC")  // IntelliJ Community
 }
+
 
 publishing {
     publications {
